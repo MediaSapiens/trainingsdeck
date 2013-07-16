273f4de745fab26f9ed7d5ed3030ac771643fd5d
igor-nov <i.v.novoseltsev@gmail.com>
2013-07-16 15:25:43 +0300

    issue 4545

---
+++ ext/yag_theme_perfectlightbox/ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "yag_theme_perfectlightbox".
+ *
+ * Auto generated 16-07-2013 15:53
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'YAG Theme PerfectLightbox',
+	'description' => 'Theme for YAG gallery that integrates Perfect Lightbox for displaying Images.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.0.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Michael Knoll, Daniel Lienert',
+	'author_email' => 'info@yag-gallery.de',
+	'author_company' => 'yag-gallery.de',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+		),
+		'conflicts' => '',
+		'suggests' => 
+		array (
+		),
+	),
+);
+
+?>
